40eb81e1421cf82f735069b56f34ecb086a8fa7b
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2024-08-01 20:32:58 -0300

    Aula02 - 01-08-2024

---
+++ Aula01/Aula01/BancoDeDados.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.SqlClient;
+
+namespace Aula01;
+
+internal class BancoDeDados
+{
+    //Usuario e Senha -> Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;
+    //Autenticacao Windows -> Server=myServerAddress;Database=myDataBase;Integrated Security=true;
+    string connectionString = "Server=MDV-NOTE;Database=AcademiaNet;Integrated Security=true;";
+    SqlConnection connection;
+
+    public BancoDeDados()
+    {
+        connection = new SqlConnection(connectionString);
+    }
+
+    public SqlConnection abrirConexao()
+    {
+        if (connection.State != System.Data.ConnectionState.Open)
+        {
+            connection.Open();
+        }
+
+        return connection;
+    }
+}
---
+++ Aula01/Aula01/Noticias.cs
@@ -0,0 +1,53 @@
+﻿using System.Data.SqlClient;
+
+namespace Aula01;
+
+internal class Noticias
+{
+    BancoDeDados bancoDeDados;
+
+    public Noticias()
+    {
+        bancoDeDados = new BancoDeDados();
+    }
+
+    private void ExibirResultado(SqlCommand command)
+    {
+        using SqlDataReader reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            Console.WriteLine("{0} - {1} - {2}", reader["NotTitulo"], reader["NotTexto"], reader["NotData"]);
+            //Console.WriteLine("{0} - {1} - {2}", reader[1], reader[2], reader[3]);
+            //Console.WriteLine(reader["NotTitulo"] + " - " + reader["NotTexto"]);
+        }
+    }
+
+    public void BuscarPorData(DateTime filtroData)
+    {
+        string sql =
+            @"SELECT NotId, NotTitulo, NotTexto, NotData, CatId
+              FROM TbNoticia
+              WHERE NotData >= @FiltroData
+              ORDER BY NotData DESC";
+
+        using SqlCommand command = new SqlCommand(sql, bancoDeDados.abrirConexao());
+        command.Parameters.AddWithValue("@FiltroData", filtroData);
+
+        ExibirResultado(command);
+    }
+
+    public void BuscarPorTitulo(string titulo)
+    {
+        string sql =
+            @"SELECT NotId, NotTitulo, NotTexto, NotData, CatId
+              FROM TbNoticia
+              WHERE UPPER(NotTitulo) LIKE UPPER('%' + @Titulo + '%')
+              ORDER BY NotData DESC";
+
+        using SqlCommand command = new SqlCommand(sql, bancoDeDados.abrirConexao());
+        command.Parameters.AddWithValue("@Titulo", titulo);
+
+        ExibirResultado(command);
+    }
+}
