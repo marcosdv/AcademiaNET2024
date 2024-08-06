fb9681fb894efb8c44e3631bcaa9d9c90fe1df7c
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2024-08-05 22:03:44 -0300

    Aula03 - 05-08-2024

---
+++ Aula01/Aula01/Noticias.cs
@@ -0,0 +1,275 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Aula01;
+
+internal class Noticias
+{
+    BancoDeDados bancoDeDados;
+
+    #region | Construtores |
+
+    public Noticias()
+    {
+        bancoDeDados = new BancoDeDados();
+    }
+
+    #endregion
+
+    #region | Metodos Basicos |
+
+    private void ExibirResultado(SqlCommand command)
+    {
+        using SqlDataReader reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            Console.WriteLine("Id: {0}\t{1}\t{2}\tCategoria:{3}\n{4}",
+                reader["NotId"], reader["NotTitulo"], reader["NotData"], reader["CatId"], reader["NotTexto"]);
+            
+            Console.WriteLine("");
+            
+            //Console.WriteLine("{0} - {1} - {2}", reader[1], reader[2], reader[3]);
+            //Console.WriteLine(reader["NotTitulo"] + " - " + reader["NotTexto"]);
+        }
+    }
+
+    #endregion
+
+    #region | Metodos de Busca |
+
+    public void BuscarPorData(DateTime filtroData)
+    {
+        string sql =
+            @"SELECT NotId, NotTitulo, NotTexto, NotData, CatId
+              FROM TbNoticia
+              WHERE NotData >= @FiltroData
+              ORDER BY NotData DESC";
+
+        try
+        {
+            using SqlCommand command = new SqlCommand(sql, bancoDeDados.abrirConexao());
+            command.Parameters.AddWithValue("@FiltroData", filtroData);
+
+            ExibirResultado(command);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Erro ao buscar por data: " + ex.Message);
+        }
+        finally
+        {
+            bancoDeDados.fecharConexao();
+        }
+    }
+
+    public void BuscarPorTitulo(string titulo)
+    {
+        string sql =
+            @"SELECT NotId, NotTitulo, NotTexto, NotData, CatId
+              FROM TbNoticia
+              WHERE UPPER(NotTitulo) LIKE UPPER('%' + @Titulo + '%')
+              ORDER BY NotData DESC";
+
+        try
+        {
+            using SqlCommand command = new SqlCommand(sql, bancoDeDados.abrirConexao());
+            command.Parameters.AddWithValue("@Titulo", titulo);
+
+            ExibirResultado(command);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Erro ao buscar por título: " + ex.Message);
+        }
+        finally
+        {
+            bancoDeDados.fecharConexao();
+        }
+    }
+
+    public void BuscarDadosViewPorCategoria(string categoria)
+    {
+        string sql = 
+            @"SELECT * FROM VwNoticia
+              WHERE UPPER(CatNome) LIKE ('%' + @Categoria + '%')";
+
+        try
+        {
+            using SqlCommand command = new SqlCommand(sql, bancoDeDados.abrirConexao());
+            command.Parameters.AddWithValue("@Categoria", categoria);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine("{0}\t{1}\t{2}\n{3}\n",
+                    reader["CatNome"], reader["NotTitulo"], reader["NotData"], reader["NotTexto"]);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Erro ao buscar na View: " + ex.Message);
+        }
+        finally
+        {
+            bancoDeDados.fecharConexao();
+        }
+    }
+
+    public void CriarDataSet()
+    {
+        DataSet dataSet = new DataSet("Noticias");
+
+        string sqlNoticia = "SELECT * FROM TbNoticia";
+        using SqlCommand commandNoticia = new SqlCommand(sqlNoticia, bancoDeDados.abrirConexao());
+
+        using SqlDataAdapter adapterNoticia = new SqlDataAdapter();
+        adapterNoticia.TableMappings.Add("TbNoticia", "TbNoticia");
+        adapterNoticia.SelectCommand = commandNoticia;
+        adapterNoticia.Fill(dataSet, "TbNoticia");
+
+        bancoDeDados.fecharConexao();
+
+        foreach (DataRow linha in dataSet.Tables["TbNoticia"].Rows)
+        {
+            foreach (DataColumn coluna in dataSet.Tables["TbNoticia"].Columns) {
+                Console.Write("{0}\t", linha[coluna]);
+            }
+            Console.WriteLine("\n");
+        }
+    }
+
+    public void CriarDataSetComCategoria()
+    {
+        SqlConnection connection = bancoDeDados.abrirConexao();
+
+        DataSet dataSet = new DataSet("Noticias");
+
+        string sqlNoticia = "SELECT * FROM TbNoticia";
+        using SqlCommand commandNoticia = new SqlCommand(sqlNoticia, connection);
+
+        using SqlDataAdapter adapterNoticia = new SqlDataAdapter();
+        adapterNoticia.TableMappings.Add("TbNoticia", "TbNoticia");
+        adapterNoticia.SelectCommand = commandNoticia;
+        adapterNoticia.Fill(dataSet, "TbNoticia");
+
+        string sqlCategoria = "SELECT * FROM TbCategoria";
+        using SqlCommand commandCategoria = new SqlCommand(sqlCategoria, connection);
+
+        using SqlDataAdapter adapterCategoria = new SqlDataAdapter();
+        adapterCategoria.TableMappings.Add("TbCategoria", "TbCategoria");
+        adapterCategoria.SelectCommand = commandCategoria;
+        adapterCategoria.Fill(dataSet, "TbCategoria");
+
+        bancoDeDados.fecharConexao();
+
+        DataColumn colunaPai = dataSet.Tables["TbCategoria"].Columns["CatId"];
+        DataColumn colunaFilho = dataSet.Tables["TbNoticia"].Columns["CatId"];
+
+        DataRelation relacionamento = new DataRelation("Categoria_Notica", colunaPai, colunaFilho);
+
+        dataSet.Relations.Add(relacionamento);
+
+        foreach (DataRow linhaCategoria in dataSet.Tables["TbCategoria"].Rows)
+        {
+            Console.WriteLine(linhaCategoria["CatNome"]);
+
+            foreach (DataRow linhaNoticia in linhaCategoria.GetChildRows(relacionamento))
+            {
+                Console.WriteLine("\t{0}", linhaNoticia["NotTitulo"]);
+            }
+        }
+    }
+
+    #endregion
+
+    #region | Manipulacao Dados |
+
+    public void Inserir(string titulo, string texto, int categoria)
+    {
+        string sql =
+            @"INSERT INTO TbNoticia (NotTitulo, NotTexto, NotData, CatId)
+                             VALUES (@NotTitulo, @NotTexto, @NotData, @CatId)";
+
+        SqlCommand command = new SqlCommand(sql, bancoDeDados.abrirConexao());
+
+        try
+        {
+            command.Parameters.AddWithValue("@NotTitulo", titulo);
+            command.Parameters.AddWithValue("@NotTexto", texto);
+            command.Parameters.AddWithValue("@NotData", DateTime.Now);
+            command.Parameters.AddWithValue("@CatId", categoria);
+
+            int numLinhas = command.ExecuteNonQuery();
+            Console.WriteLine("Número de linhas afetadas: " + numLinhas);
+        }
+        catch (SqlException ex)
+        {
+            Console.WriteLine("Erro ao inserir!\n" + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Erro ao inserir!\n" + ex.Message);
+        }
+        finally
+        {
+            command.Dispose();
+            bancoDeDados.fecharConexao();
+        }
+    }
+
+    public void Alterar(int codigo, string titulo, string texto, int categoria)
+    {
+        string sql =
+            @"UPDATE TbNoticia SET
+                NotTitulo = @NotTitulo,
+                NotTexto = @NotTexto,
+                CatId = @CatId
+              WHERE NotId = @NotId";
+
+        try
+        {
+            using SqlCommand command = new SqlCommand(sql, bancoDeDados.abrirConexao());
+            command.Parameters.AddWithValue("@NotTitulo", titulo);
+            command.Parameters.AddWithValue("@NotTexto", texto);
+            command.Parameters.AddWithValue("@CatId", categoria);
+            command.Parameters.AddWithValue("@NotId", codigo);
+
+            int numLinhas = command.ExecuteNonQuery();
+            Console.WriteLine("Número de linhas afetadas: " + numLinhas);
+        }
+        catch (SqlException ex)
+        {
+            Console.WriteLine("Erro ao alterar!\n" + ex.Message);
+        }
+        finally
+        {
+            bancoDeDados.fecharConexao();
+        }
+    }
+
+    public void Excluir(int codigo)
+    {
+        string sql = "DELETE FROM TbNoticia WHERE NotId = @NotId";
+
+        try
+        {
+            using SqlCommand command = new SqlCommand(sql, bancoDeDados.abrirConexao());
+            command.Parameters.AddWithValue("NotId", codigo);
+
+            int numLinhas = command.ExecuteNonQuery();
+            Console.WriteLine("Número de linhas afetadas: " + numLinhas);
+        }
+        catch (SqlException ex)
+        {
+            Console.WriteLine("Erro ao excluir!\n" + ex.Message);
+        }
+        finally
+        {
+            bancoDeDados.fecharConexao();
+        }
+    }
+
+    #endregion
+}
---
+++ Aula01/Aula01/Program.cs
@@ -0,0 +1,110 @@
+﻿using Aula01;
+using System.Data.SqlClient;
+
+internal class Program
+{
+    private static Noticias noticias;
+
+    private static void Main(string[] args)
+    {
+        noticias = new Noticias();
+        int opcao = 0;
+
+        while (opcao != 9)
+        {
+            Console.WriteLine("Selecione a opção de Filtro:");
+            Console.WriteLine("1 -> Filtrar por Número de dias");
+            Console.WriteLine("2 -> Filtrar por Título");
+            Console.WriteLine("3 -> Inserir Notícia");
+            Console.WriteLine("4 -> Alterar Notícia");
+            Console.WriteLine("5 -> Excluir Notícia");
+            Console.WriteLine("6 -> Buscar por Categoria");
+            Console.WriteLine("7 -> Listar dados do DataSet");
+            Console.WriteLine("8 -> Listar dados por Categoria");
+            Console.WriteLine("9 -> Encerrar Aplicação");
+            opcao = int.Parse(Console.ReadLine());
+            
+            Console.Clear();
+
+            switch (opcao)
+            {
+                case 1: BuscaPorData(); break;
+                case 2: BuscaPorTitulo(); break;
+                case 3: InserirNoticia(); break;
+                case 4: AlterarNoticia(); break;
+                case 5: ExcluirNoticia(); break;
+                case 6: BuscaPorCategoria(); break;
+                case 7: noticias.CriarDataSet(); break;
+                case 8: noticias.CriarDataSetComCategoria(); break;
+                case 9: break;
+                default: Console.WriteLine("Opção inválida!"); break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    private static void BuscaPorData()
+    {
+        Console.Write("Digite até quantos dias busca a notícia: ");
+        int numDias = int.Parse(Console.ReadLine()) * -1;
+        DateTime filtroData = DateTime.Now.Date.AddDays(numDias);
+
+        noticias.BuscarPorData(filtroData);
+    }
+
+    private static void BuscaPorTitulo()
+    {
+        Console.Write("Filtrar por título da notícia: ");
+        string titulo = Console.ReadLine();
+
+        noticias.BuscarPorTitulo(titulo);
+    }
+
+    private static void BuscaPorCategoria()
+    {
+        Console.Write("Filtrar por nome da categoria: ");
+        string categoria = Console.ReadLine();
+
+        noticias.BuscarDadosViewPorCategoria(categoria);
+    }
+
+    private static void InserirNoticia()
+    {
+        Console.Write("Título da notícia: ");
+        string titulo = Console.ReadLine();
+
+        Console.Write("Texto da notícia: ");
+        string texto = Console.ReadLine();
+
+        Console.Write("Categoria da Notícia: ");
+        int categoria = int.Parse(Console.ReadLine());
+
+        noticias.Inserir(titulo, texto, categoria);
+    }
+
+    private static void AlterarNoticia()
+    {
+        Console.Write("Notícia que deseja alterar: ");
+        int codigo = int.Parse(Console.ReadLine());
+
+        Console.Write("Título da notícia: ");
+        string titulo = Console.ReadLine();
+
+        Console.Write("Texto da notícia: ");
+        string texto = Console.ReadLine();
+
+        Console.Write("Categoria da Notícia: ");
+        int categoria = int.Parse(Console.ReadLine());
+
+        noticias.Alterar(codigo, titulo, texto, categoria);
+    }
+
+    private static void ExcluirNoticia()
+    {
+        Console.Write("Notícia que deseja excluir: ");
+        int codigo = int.Parse(Console.ReadLine());
+
+        noticias.Excluir(codigo);
+    }
+}
