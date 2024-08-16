4bbb46c468207809ca63051cc30b2b0b6b469794
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2024-08-15 22:03:44 -0300

    Aula08 - 15-08-2024

---
+++ Aula04/Aula04/Controllers/NoticiaController.cs
@@ -0,0 +1,27 @@
+﻿using Aula04.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aula04.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoticiaController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult BuscarTodas([FromServices] IConfiguration configuration)
+        {
+            NoticiaRepository repository = new NoticiaRepository(configuration);
+            var lista = repository.BuscarTodas();
+            return Ok(lista);
+        }
+
+        [HttpGet("ComCategoria")]
+        public IActionResult BuscarTodosComCategoria([FromServices] IConfiguration configuration)
+        {
+            NoticiaRepository repository = new NoticiaRepository(configuration);
+            var lista = repository.BuscarTodosComCategoria();
+            return Ok(lista);
+        }
+    }
+}
---
+++ Aula04/Aula04/Repositories/NoticiaRepository.cs
@@ -0,0 +1,36 @@
+﻿using Aula04.Models;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+namespace Aula04.Repositories
+{
+    public class NoticiaRepository
+    {
+        private readonly SqlConnection connection;
+
+        public NoticiaRepository(IConfiguration configuration)
+        {
+            string connectionString = configuration.GetConnectionString("conexao") ?? throw new Exception("ConnectionString sem valor válido");
+            connection = new SqlConnection(connectionString);
+        }
+
+        public IEnumerable<Noticia> BuscarTodas()
+        {
+            return connection.Query<Noticia>("SELECT * FROM TbNoticia");
+        }
+
+        public IEnumerable<Noticia> BuscarTodosComCategoria()
+        {
+            string sql = "SELECT N.*, C.CatNome FROM TbNoticia N INNER JOIN TbCategoria C ON C.CatId = N.CatId";
+
+            return connection.Query<Noticia, Categoria, Noticia>(sql,
+                (noticia, categoria) => {
+                    noticia.Categoria = categoria;
+                    return noticia;
+                },
+                splitOn: "CatId"
+            );
+        }
+    }
+}
