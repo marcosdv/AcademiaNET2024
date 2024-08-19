f414b783b1f6a4f27769a446c31820a3ae2f89be
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2024-08-19 20:49:10 -0300

    Aula09 - 19-08-2024

---
+++ Aula04/Aula04/Controllers/NoticiaController.cs
@@ -0,0 +1,76 @@
+﻿using Aula04.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Aula04.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoticiaController : ControllerBase
+    {
+        private readonly NoticiaRepository repository;
+
+        public NoticiaController(IConfiguration configuration)
+        {
+            repository = new NoticiaRepository(configuration);
+        }
+
+        [HttpGet]
+        //public IActionResult BuscarTodas([FromServices] IConfiguration configuration)
+        public IActionResult BuscarTodas()
+        {
+            try
+            {
+                var lista = repository.BuscarTodas();
+                return Ok(lista);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("ComCategoria")]
+        public IActionResult BuscarTodosComCategoria()
+        {
+            try
+            {
+                var lista = repository.BuscarTodosComCategoria();
+                return Ok(lista);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("{codigo}")]
+        public IActionResult BuscarPorId(int codigo)
+        {
+            try
+            {
+                var noticia = repository.BuscarPorId(codigo);
+                return Ok(noticia);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("Categoria/{codigo}")]
+        public IActionResult BuscarPorCategoria(int codigo)
+        {
+            try
+            {
+                var lista = repository.BuscarPorCategoria(codigo);
+                return Ok(lista);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
---
+++ Aula04/Aula04/Repositories/NoticiaRepository.cs
@@ -0,0 +1,77 @@
+﻿using Aula04.Models;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace Aula04.Repositories
+{
+    public class NoticiaRepository
+    {
+        private readonly SqlConnection connection;
+
+        public NoticiaRepository(IConfiguration configuration)
+        {
+            string connectionString = configuration.GetConnectionString("conexao") ?? throw new Exception("ConnectionString sem valor válido");
+            connection = new SqlConnection(connectionString);
+        }
+
+        public IEnumerable<Noticia> BuscarTodas()
+        {
+            return connection.Query<Noticia>("SELECT * FROM TbNoticia");
+        }
+
+        public IEnumerable<Noticia> BuscarTodosComCategoria()
+        {
+            string sql = "SELECT N.*, C.CatNome FROM TbNoticia N INNER JOIN TbCategoria C ON C.CatId = N.CatId";
+
+            return connection.Query<Noticia, Categoria, Noticia>(sql,
+                (noticia, categoria) => {
+                    noticia.Categoria = categoria;
+                    return noticia;
+                },
+                splitOn: "CatId"
+            );
+        }
+
+        public Noticia? BuscarPorId(int codigo)
+        {
+            string sql = @"
+                SELECT N.*, C.CatNome
+                FROM TbNoticia N
+                INNER JOIN TbCategoria C ON C.CatId = N.CatId
+                WHERE N.NotId = @NotId
+            ";
+
+            var parametros = new { NotId = codigo };
+
+            var lista = connection.Query<Noticia, Categoria, Noticia>(sql,
+                (noticia, categoria) => {
+                    noticia.Categoria = categoria;
+                    return noticia;
+                },
+                splitOn: "CatId",
+                param: parametros
+            );
+
+            return lista.FirstOrDefault();
+        }
+
+        public IEnumerable<Noticia> BuscarPorCategoria(int codigo)
+        {
+            string sql = @"
+                SELECT N.*, C.CatNome
+                FROM TbNoticia N
+                INNER JOIN TbCategoria C ON C.CatId = N.CatId
+                WHERE C.CatId = @CatId
+            ";
+
+            return connection.Query<Noticia, Categoria, Noticia>(sql,
+                (noticia, categoria) => {
+                    noticia.Categoria = categoria;
+                    return noticia;
+                },
+                splitOn: "CatId",
+                param: new { CatId = codigo }
+            );
+        }
+    }
+}
