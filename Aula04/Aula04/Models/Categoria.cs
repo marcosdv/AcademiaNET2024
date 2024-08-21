5807e9abaf01396f5b7e9671167652ff4ca86f87
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2024-08-21 20:50:29 -0300

    Aula10 - 21-08-2024

---
+++ Aula04/Aula04/Models/Categoria.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Aula04.Models
+{
+    public class Categoria
+    {
+        public int CatId { get; set; }
+        public string CatNome { get; set; }
+
+        public Categoria() { }
+
+        public Categoria(SqlDataReader reader)
+        {
+            CatId = reader.GetInt32("CatId");
+            CatNome = reader.GetString("CatNome");
+        }
+
+        public Categoria(int codigo, CategoriaRequest request)
+        {
+            CatId = codigo;
+            CatNome = request.Nome;
+        }
+    }
+}
