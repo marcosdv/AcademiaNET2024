d5b02e02f6b09037762c9276609c3706a858f4f1
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2024-08-21 22:03:02 -0300

    Aula10 - 21-08-2024

--- Aula04/Aula04/Models/NoticiaRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Aula04.Models
-{
-    public class NoticiaRequest
-    {
-        public string Titulo { get; set; }
-        public string Texto { get; set; }
-        public int CodCategoria { get; set; }
-    }
-}
