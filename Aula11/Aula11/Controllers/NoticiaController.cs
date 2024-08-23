451c04724c23e6953c59e8118c92c812e72987e2
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2024-08-22 22:01:25 -0300

    Aula11 - 22-08-2024

---
+++ Aula11/Aula11/Controllers/NoticiaController.cs
@@ -0,0 +1,45 @@
+﻿using Aula11.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aula11.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoticiaController : ControllerBase
+    {
+        private readonly NoticiaRepository repository;
+
+        public NoticiaController(IConfiguration config)
+        {
+            repository = new NoticiaRepository(config);
+        }
+
+        [HttpGet]
+        public IActionResult BuscarTodas()
+        {
+            try
+            {
+                var lista = repository.BuscarTodas();
+                return Ok(lista);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("{codigo}")]
+        public IActionResult BuscarPorId(int codigo)
+        {
+            try
+            {
+                var noticia = repository.BuscarPorId(codigo);
+                return Ok(noticia);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Aula11/Aula11/Repositories/NoticiaRepository.cs
@@ -0,0 +1,37 @@
+﻿using Aula11.Models;
+using Aula11.Repositories.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aula11.Repositories
+{
+    public class NoticiaRepository
+    {
+        private readonly DataContext context;
+
+        public NoticiaRepository(IConfiguration configuration)
+        {
+            var options = new DbContextOptionsBuilder<DataContext>();
+            options.UseSqlServer(configuration.GetConnectionString("MyConnection"));
+
+            context = new DataContext(options.Options);
+        }
+
+        public IEnumerable<Noticia> BuscarTodas()
+        {
+            return context.Noticias;
+        }
+
+        public Noticia? BuscarPorId(int id)
+        {
+            /*
+            return (
+                from noticia in context.Noticias
+                where noticia.Id == id
+                select noticia
+            ).FirstOrDefault();
+            */
+
+            return context.Noticias.Where(x => x.Id == id).FirstOrDefault();
+        }
+    }
+}
